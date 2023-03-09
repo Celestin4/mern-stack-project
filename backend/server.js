ca5fbcb0b11f6df47883b6337be9b23ed55ef102
Celestin4 <nteziryayocelestin3@gmail.com>
2023-03-09 20:28:08 +0200

    establishing sever and routers

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log("hello world")
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 3500
+const app = express()
+
+// app.use('/api/goals', require('./routes/goalRoutes'))
+app.get('/api/goals', (req, res) => {
+    res.send('is harsh')
+});
+
+app.listen(port, () => console.log(`Server is started on port ${port}`))
